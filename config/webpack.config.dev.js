8865d53edbdd902d87e35b3ed4f395efd28f06d4
杜晓天 <dxt_web@qq.com>
2021-11-21 22:51:26 +0800

    区分生产环境和开发环境

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,11 +1,151 @@
 const { merge } = require('webpack-merge')
 const BaseWebpackConfig = require('../webpack.config.base')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path')
 module.exports = merge(BaseWebpackConfig, {
   mode: 'development',
+  entry: {
+    // SDK入口文件
+    SDK: path.join(__dirname, '../SDK/main.js'),
+    // vue入口文件
+    Vue: path.join(__dirname, '../src/main.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    libraryTarget: 'this',
+    globalObject: 'this',
+    publicPath: '/',
+  },
   devServer: {
     compress: true,
     port: 3000,
     open: true,
     hot: true,
   },
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.runtime.esm.js',
+    },
+    extensions: ['.js', '.vue'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'cache-loader',
+          },
+          {
+            loader: 'thread-loader',
+          },
+          {
+            loader: 'vue-loader',
+            options: {
+              compilerOptions: {
+                preserveWhitespace: false,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('dart-sass'),
+            },
+          },
+          {
+            loader: 'postcss-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 4096,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: 'img/[name].[hash:8].[ext]',
+                },
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 4096,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: 'media/[name].[hash:8].[ext]',
+                },
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 4096,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: 'fonts/[name].[hash:8].[ext]',
+                },
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'SDK案例',
+      template: path.join(__dirname, '../public/index.html'),
+      inject: 'body',
+      scriptLoading: 'blocking',
+    }),
+  ],
 })
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,5 +1,23 @@
 const { merge } = require('webpack-merge')
 const BaseWebpackConfig = require('../webpack.config.base')
+const path = require('path')
 module.exports = merge(BaseWebpackConfig, {
   mode: 'production',
+  entry: ['@babel/polyfill', path.join(__dirname, '../SDK/main.js')],
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'main.js',
+    libraryTarget: 'this',
+    globalObject: 'this',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: { loader: 'babel-loader' },
+        exclude: '/node_modules/',
+      },
+    ],
+  },
 })
