dd2491838bea1c02ea02a77a0999239115220e0a
杜晓天 <dxt_web@qq.com>
2021-11-21 22:40:44 +0800

    添加Vue调试环境

---
+++ SDK/main.js
@@ -0,0 +1,15 @@
+export class Phone {
+  constructor(name) {
+    console.log(`我是SDK文件中的class${name}`)
+  }
+}
+export class WeChat {
+  constructor() {
+    console.log('WeChat')
+  }
+}
+export class Message {
+  constructor() {
+    console.log('Message')
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,17 +1,12 @@
-export default {
-  Phone: class Phone {
-    constructor() {
-      console.log('Phone')
-    }
-  },
-  WeChat: class WeChat {
-    constructor() {
-      console.log('WeChat')
-    }
-  },
-  Message: class Message {
-    constructor() {
-      console.log('Message')
-    }
-  },
-}
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount('#app')
